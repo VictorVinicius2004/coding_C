eada832f8b42056fdb6ce755c5a964c6950f0005
Victor Vinicius <vvfs2004@gmail.com>
2025-01-29 20:14:10 -0300

    C

--- atividades_ED1/desafio1_prova1.c
+++ atividades_ED1/desafio1_prova1.c
@@ -48,43 +48,38 @@ int main(){
 	printf("\n\nMaior diferença entre valores consecutivos: \n[%02d, %02d]\n", vetor[pos], vetor[pos+1]);
 	
 	//problema 2
-	int pos1=0, pos1_atual=0, pos2=0, pos2_atual=0;
-	for(int i=1; i<n; i++){
-		int soma=0, soma_atual=0;
-		if(vetor[i]>vetor[i-1])
-			pos2_atual=i;
+	int pos1=0, pos1_atual=0, pos2=0, pos2_atual=0, soma=0, soma_atual=0;
+	for(int i=0; i<n-1; i++){
+		soma_atual+= vetor[i]; 
+		
+		if(vetor[i]<vetor[i+1]){ 
+			pos2_atual=i+1;
 			
-		if(vetor[i]<vetor[i-1] || i==n-1){
-			diff=pos2-pos1;
-			diff_atual=pos2_atual-pos1_atual;
-			
-			if(diff_atual>diff){
-				pos1=pos1_atual;
-				pos2=pos2_atual;
-			}
-			if(diff==diff_atual && diff){
-				for(int j=pos1; j<=pos2; j++)
-					soma+=vetor[j];
-				for(int j=pos1_atual; j<=pos2_atual; j++)
-					soma_atual+=vetor[j];
-				
-				if(soma_atual>soma){
-					pos1=pos1_atual;
-					pos2=pos2_atual;
-				}
-			}
-			
-			pos1_atual=i;
+			if(i==n-2 && n!=1)
+				soma_atual+=vetor[i+1];
+		}
+		
+		if(pos2_atual-pos1_atual>pos2-pos1){
+			soma=soma_atual;
+			pos1=pos1_atual;
+			pos2=pos2_atual;
+		}
+		else if(pos2_atual-pos1_atual==pos2-pos1 && soma_atual>soma){
+			pos1=pos1_atual;
+			pos2=pos2_atual;
+			soma=soma_atual;
+		}
+		
+		if(vetor[i]>vetor[i+1]){	
+			pos1_atual=i+1; 
+			soma_atual=0;
 		}
 	}
 	
-	printf("\n\nMaior sequência crescente: \n[");
-	for(int i=pos1; i<=pos2; i++){
-		if(i<pos2)
-			printf("%02d, ", vetor[i]);
-		else
-			printf("%02d]", vetor[i]);
-	}
+	printf("\n\nMaior sequência crescente:\n");
+	for(int i=pos1; i<=pos2; i++)
+		printf("[%02d] ", vetor[i]);
+	
 	return 0;
 }
 
---
+++ atividades_ED1/desafio2_prova1.c
@@ -0,0 +1,47 @@
+/*O h-index é uma métrica bastante famosa para qualificar a 
+produtividade e o impacto de cientistas ao redor do mundo, 
+baseando-se na quantidade e qualidade dos artigos científicos produzidos. 
+O h-index é calculado através da quantidade de artigos com 
+número de citações maiores ou iguais a essa quantidade. Considerando isso, 
+faça um programa que dada uma quantidade N de artigos, sorteie N valores que 
+representam o número de citações de cada um destes artigos. Os valores sorteados 
+devem ser no intervalo entre 0 e N. Imprima o array sorteado e calcule o h-index 
+desse cenário.
+
+P.Ex.: [3,1,6,7,3,2,2,3,4,6]
+h-index:    4
+Explicação: Existem pelo menos 4 artigos com número de citações maior ou igual a 4.*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+	srand(time(NULL));
+	int n;
+	printf("Digite o número de artigos.\n");
+	scanf(" %d", &n);
+	
+	//preenchendo o vetor
+	int vetor[n];
+	for(int i=0; i<n; i++){
+		vetor[i]=rand()%n;
+		printf("%02d, ", vetor[i]);
+	}printf("\n");
+		
+	//h-index
+	int hindex=-1;
+	for(int i=0; i<n; i++){
+		int cont=0;
+
+		for(int j=0; j<n; j++)
+			if(vetor[j]>=vetor[i])
+				cont++;
+		
+		if(cont>=vetor[i] && vetor[i]>hindex)
+			hindex=vetor[i];
+	}
+	
+	printf("h-index: %d \nExistem pelo menos %d artigos com número de citações maiores ou iguais a %d", hindex, hindex, hindex);
+	return 0;
+}
