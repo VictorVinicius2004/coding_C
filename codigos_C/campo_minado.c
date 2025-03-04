c9963599999d67cae0ee1b67a88e5c12d10de7c7
Victor Vinicius <vvfs2004@gmail.com>
2025-03-04 16:34:03 -0300

    C

--- codigos_C/campo_minado.c
+++ codigos_C/campo_minado.c
@@ -58,31 +58,47 @@ void mostrar_bombas(){
 	for(int i=0; i<LINHAS; i++){
 		for(int j=0; j<COLUNAS; j++){
 			if(campo[i][j].bomba==CHEIO)
-				printf("%s", VERMELHO);
+				printf("%s#%s ", VERMELHO, FIM_COR);
+			else if(campo[i][j].bombas_proximas>0)
+				printf("%d ", campo[i][j].bombas_proximas);
 			else
-				printf("%s", VERDE);
-			printf("#%s ", FIM_COR);
+				printf("  ");
 		}
 		printf("\n");
 	}
 }
 
-void opcao(char escolha){
+int marcas_proximas(){
+	int min_linha = (posicao.linha-1>=0)? posicao.linha-1 : 0;
+	int min_coluna = (posicao.coluna-1>=0)? posicao.coluna-1 : 0;
+	int max_linha = (posicao.linha+1<LINHAS)? posicao.linha+1 : LINHAS-1;
+	int max_coluna = (posicao.coluna+1<COLUNAS)? posicao.coluna+1 : COLUNAS-1;
+	
+	int cont=0;
+	for(int i=min_linha; i<=max_linha; i++)
+		for(int j=min_coluna; j<=max_coluna; j++)
+			if((i!=0 || j!=0) && campo[i][j].estado==MARCADO)
+				cont++;
+	
+	return cont;
+}
+
+void opcao(char escolha, int linha, int coluna){
 	switch(escolha){
-		case '1':
-			if(campo[posicao.linha][posicao.coluna].bomba==LIMPO)
-				revelar(posicao.linha, posicao.coluna);
+		case '1'://revelar
+			if(campo[linha][coluna].bomba==LIMPO)
+				revelar(linha, coluna);
 			else
 				perdeu=1;
 			break;
-		case '2':
-			if(campo[posicao.linha][posicao.coluna].estado!=VISIVEL){
-				if(campo[posicao.linha][posicao.coluna].estado==MARCADO){
-					campo[posicao.linha][posicao.coluna].estado=NEUTRO;
+		case '2'://marcar ou desmarcar
+			if(campo[linha][coluna].estado!=VISIVEL){
+				if(campo[linha][coluna].estado==MARCADO){
+					campo[linha][coluna].estado=NEUTRO;
 					marcacoes--;
 					break;
 				}
-				campo[posicao.linha][posicao.coluna].estado=MARCADO;
+				campo[linha][coluna].estado=MARCADO;
 				marcacoes++;
 			}
 	}
@@ -108,9 +124,26 @@ int selecao(){
 			posicao.coluna = (posicao.coluna+1<COLUNAS)? posicao.coluna+1 : COLUNAS-1;
 			break;
 		case '1':
+			if(!inicializado)
+				return 1;
+			
+			//caso o jogador queira revelar todas as casas ao redor da posição escolhida
+			//a posição ja deve estar revelada e deve ter o número de marcações ao seu redor igual ao número de bombas próximas
+			if(campo[posicao.linha][posicao.coluna].estado==VISIVEL && 
+			marcas_proximas()==campo[posicao.linha][posicao.coluna].bombas_proximas){
+				int min_linha = (posicao.linha-1>=0)? posicao.linha-1 : 0;
+				int min_coluna = (posicao.coluna-1>=0)? posicao.coluna-1 : 0;
+				int max_linha = (posicao.linha+1<LINHAS)? posicao.linha+1 : LINHAS-1;
+				int max_coluna = (posicao.coluna+1<COLUNAS)? posicao.coluna+1 : COLUNAS-1;
+				
+				for(int i=min_linha; i<=max_linha; i++)
+					for(int j=min_coluna; j<=max_coluna; j++)
+						if((i!=posicao.linha || j!=posicao.coluna) && campo[i][j].estado==NEUTRO)
+							opcao(direcao, i, j);
+			}
 		case '2':
-			if(inicializado)
-				opcao(direcao);
+			opcao(direcao, posicao.linha, posicao.coluna);
+				
 			return 1;
 	}
 	return 0;
@@ -122,7 +155,7 @@ void interface(){
 		printf("BOMBAS: %d       MARCAÇÕES: %d\n", BOMBAS, marcacoes);
 		for(int i=0; i<LINHAS; i++){
 			for(int j=0; j<COLUNAS; j++){
-				if(inicializado){
+				if(inicializado){//se o campo já foi inicializado
 					if(i==posicao.linha && j==posicao.coluna)
 						printf("%s",AZUL);
 					if(campo[i][j].estado==VISIVEL){
@@ -160,34 +193,35 @@ void interface(){
 	}while(1);
 }
 
-void inicializa_campo(){
-	//trata o lixo no campo
+void tratar_lixo_no_campo(){
 	for(int i=0; i<LINHAS; i++)
 		for(int j=0; j<COLUNAS; j++){
 			campo[i][j].bomba=LIMPO;
 			campo[i][j].bombas_proximas=0;
 			campo[i][j].estado=NEUTRO;
 		}
-	
-	//coloca as bombas
+}
+
+void colocar_bombas(){
 	int cont=0;
 	do{
 		int l = rand()%LINHAS;
 		int c = rand()%COLUNAS;
 		
 		//se não estiver perto do ponto inicial 
-		//e nem a posicao escolhida já tiver uma bomba, uma nova bomba é posicionada
+		//e a posicao escolhida não tiver nenhuma bomba, uma nova bomba é posicionada
 		if(!((l>=posicao.linha-2 && l<=posicao.linha+2) && (c>=posicao.coluna-2 && c<=posicao.coluna+2)) &&
 		campo[l][c].bomba==LIMPO){
 			cont++;
 			campo[l][c].bomba=CHEIO;
 		}
 	}while(cont<BOMBAS);
-	
-	//calcula o número de bombas próximas de cada casa
+}
+
+void calcular_bombas_proximas(){
 	for(int i=0; i<LINHAS; i++)
 		for(int j=0; j<COLUNAS; j++){
-			//calcula a área que será analizada evitando áreas fora do campo
+			//delimita a área que será analizada evitando áreas fora do campo
 			int min_linha = (i-1>=0)? i-1 : 0;
 			int min_coluna = (j-1>=0)? j-1 : 0;
 			int max_linha = (i+1<LINHAS)? i+1 : LINHAS-1;
@@ -199,6 +233,12 @@ void inicializa_campo(){
 					if(campo[l][c].bomba==CHEIO && (l!=i || c!=j))
 						campo[i][j].bombas_proximas++;
 				}
+}
+
+void inicializa_campo(){
+	tratar_lixo_no_campo();
+	colocar_bombas();
+	calcular_bombas_proximas();
 }
 
 int main(){
