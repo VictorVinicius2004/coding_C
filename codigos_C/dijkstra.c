eed7fd9583d3bd7518faa141e90eaff801d2ccc2
Victor Vinicius <vvfs2004@gmail.com>
2025-06-03 08:02:10 -0300

    C

--- codigos_C/dijkstra.c
+++ codigos_C/dijkstra.c
@@ -2,7 +2,9 @@
 
 #define VERTICES 4
 #define INFINITO 2147483647
-enum {VERDADEIRO, FALSO};
+#define VERDADEIRO 1
+#define FALSO 0
+#define INDEFINIDO -1
 
 typedef struct{
 	int destino;
@@ -15,27 +17,51 @@ void inicializaTabela(Tabela tabela[]){
 	for(int i=0; i<VERTICES; i++){
 		tabela[i].destino=i;
 		tabela[i].visitado=FALSO;
+		tabela[i].origem=INDEFINIDO;
 		tabela[i].peso = (i==0)? 0 : INFINITO;
 	}
 }
 
-void dijkstra(int grafo[VERTICES][VERTICES],int origem,Tabela tabela[]){
-	for(int destino=0; destino<VERTICES; destino++){
-		if(grafo[origem][destino]==0)
-			continue;
-		
-		int alterado=FALSO;
-		tabela[origem].visitado=VERDADEIRO;
-		
-		int pesoAtual = grafo[origem][destino] + tabela[origem].peso;
-		if(pesoAtual<tabela[destino].peso){
-			tabela[destino].peso=pesoAtual;
-			tabela[destino].origem=origem;
-			alterado=VERDADEIRO;
+void printTabela(Tabela tabela[]){
+	for(int i=0; i<VERTICES; i++){
+		printf("%02d | ",tabela[i].destino);
+		if(tabela[i].peso==INFINITO)
+			printf("∞  | ");
+		else
+			printf("%02d | ", tabela[i].peso);
+		if(tabela[i].origem!=INDEFINIDO)
+			printf("%02d",tabela[i].origem);
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void dijkstra(int grafo[VERTICES][VERTICES],int origem, int final, Tabela tabela[]){
+	for(int origem=0; origem<VERTICES; origem++){
+		for(int destino=0; destino<VERTICES; destino++){
+			if(grafo[origem][destino]==0)
+				continue;
+			if(origem==final)
+				return;
+				
+			int alterado=FALSO;
+			tabela[origem].visitado=VERDADEIRO;
+			
+			int pesoAtual = grafo[origem][destino] + tabela[origem].peso;
+			if(pesoAtual<tabela[destino].peso){
+				tabela[destino].peso=pesoAtual;
+				tabela[destino].origem=origem;
+				alterado=VERDADEIRO;
+			}
+			
+			printf("{%d --> %d}\n",origem,destino);
+			printTabela(tabela);
+			
+			if(alterado==VERDADEIRO && tabela[destino].visitado==VERDADEIRO){
+				origem=destino;
+				destino=0;
+			}
 		}
-		
-		if(alterado==VERDADEIRO || tabela[destino].visitado==FALSO)
-			dijkstra(grafo,destino,tabela);
 	}
 }
 
@@ -56,15 +82,15 @@ void printCaminho(Tabela tabela[], int origem, int destino){
 int main(){
 	int grafo[VERTICES][VERTICES]={
 	  // 0 1 2 3
-		{0,3,4,0},//0
-		{3,0,1,3},//1
-		{4,1,0,2},//2
-		{0,3,2,0},//3
+		{0,6,4,0},//0
+		{6,0,1,2},//1
+		{4,1,0,4},//2
+		{0,2,4,0},//3
 	};
 	Tabela tabela[VERTICES];
 	inicializaTabela(tabela);
 	
-	dijkstra(grafo,0,tabela);
+	dijkstra(grafo,0,3,tabela);
 	printCaminho(tabela,0,3);
 	return 0;
 }
